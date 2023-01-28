9080cb122cd580cb87d73fab4a36f842cb4a2a1d
abkubatt <abkubatt@gmail.com>
2023-01-28 11:43:54 +0600

    Initial Commit

---
+++ Manas-Menu/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Manas-Menu
+//
+//  Created by Abdulmajit Kubatbekov on 28.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
