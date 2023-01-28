78409e9da13b427552f8a4d163651e5932c6a2b2
abkubatt <abkubatt@gmail.com>
2023-01-28 17:32:01 +0600

    added scrollView

--- Manas-Menu/Controllers/Core/HomeViewController.swift
+++ Manas-Menu/Controllers/Core/HomeViewController.swift
@@ -8,12 +8,51 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private let scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.backgroundColor = .green
+        scroll.translatesAutoresizingMaskIntoConstraints = false
+        return scroll
+    }()
+    
+    private let imageView: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "arka")
+        image.contentMode = .scaleAspectFit
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Home"
         view.backgroundColor = .systemBackground
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
 
+        congfigureConstraints()
+    }
+    
+    
+    private func congfigureConstraints() {
+        
+        let scrollViewConstraints = [
+            scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 200)
+        ]
+        
+        let imageViewContraints = [
+            imageView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            imageView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(scrollViewConstraints)
+        NSLayoutConstraint.activate(imageViewContraints)
     }
 
 }
