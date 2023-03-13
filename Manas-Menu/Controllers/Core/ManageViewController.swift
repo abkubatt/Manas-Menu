bd91555f0bd1bce288898b3e1ae85423454eb0c0
abkubatt <abkubatt@gmail.com>
2023-03-13 14:43:15 +0600

    add list of admin actions

--- Manas-Menu/Controllers/Core/ManageViewController.swift
+++ Manas-Menu/Controllers/Core/ManageViewController.swift
@@ -9,13 +9,90 @@ import UIKit
 
 
 class ManageViewController: UIViewController {
-       
+    
+    private let images: [UIImage] = [
+        UIImage(systemName: "calendar.badge.plus")!,
+        UIImage(systemName: "square.and.pencil")!,
+        UIImage(systemName: "trash")!,
+        UIImage(systemName: "cup.and.saucer")!,
+        UIImage(systemName: "square.and.pencil")!,
+        UIImage(systemName: "trash")!,
+        UIImage(systemName: "fork.knife")!,
+        UIImage(systemName: "square.and.pencil")!,
+        UIImage(systemName: "trash")!,
+        
+    ]
+    
+    private let titles: [String] = ["Add menu", "Update menu", "Delete menu", "Add canteen foods", "Update canteen foods", "Delete canteen foods", "Add free foods", "Update free foods", "Delete free foods"]
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .systemBackground
+        tableView.allowsSelection = true
+        tableView.register(ManageListTableViewCell.self, forCellReuseIdentifier: ManageListTableViewCell.identifier)
+        return tableView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Manage"
         view.backgroundColor = .systemBackground
+        
+        self.view.addSubview(tableView)
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+
+        configureConstraints()
+
     }
 
+    
+    private func configureConstraints() {
+        
+        let tableViewContraints = [
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(tableViewContraints)
+
+    }
+
+
+}
+
+
+extension ManageViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: ManageListTableViewCell.identifier, for: indexPath) as? ManageListTableViewCell else{
+            fatalError("The TableView could not dequeue a ManageListUITableViewCell in ManageViewController.")
+        }
+        
+        let image = self.images[indexPath.row]
+        cell.configure(with: image, and: titles[indexPath.row])
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 40
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+        print("DEBUG PRINT:", indexPath.row)
+    }
+    
+    
 }
 
 
---
+++ Manas-Menu/Views/ManageListTableViewCell.swift
@@ -0,0 +1,94 @@
+//
+//  ManageListTableViewCell.swift
+//  Manas-Menu
+//
+//  Created by Abdulmajit Kubatbekov on 13.03.23.
+//
+
+import UIKit
+
+class ManageListTableViewCell: UITableViewCell {
+
+    static let identifier = "ManageListTableViewCell"
+    
+    private let iconOfTitle: UIImageView = {
+        let image = UIImageView()
+        image.contentMode = .scaleAspectFill
+        image.image = UIImage(systemName: "questionmark")
+        image.tintColor = .label
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    private let titles: UILabel = {
+        let label = UILabel()
+        label.textColor = .label
+        label.textAlignment = .left
+        label.font = .systemFont(ofSize: 15, weight: .light)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "Error"
+        return label
+    }()
+    
+    private let rightArrow: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(systemName: "chevron.right")
+        image.tintColor = .secondaryLabel
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init coder")
+    }
+    
+    public func configure(with image: UIImage, and label: String) {
+        iconOfTitle.image = image
+        titles.text = label
+    }
+    
+    private func setupUI() {
+        
+        contentView.addSubview(iconOfTitle)
+        contentView.addSubview(titles)
+        contentView.addSubview(rightArrow)
+        
+        configureConstraints()
+        
+    }
+    
+    private func configureConstraints(){
+        
+        let myimageConstraints = [
+            iconOfTitle.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+            iconOfTitle.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor),
+            iconOfTitle.leadingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.leadingAnchor),
+            iconOfTitle.widthAnchor.constraint(equalToConstant: 20)
+        ]
+        
+        let mylabelConstraints = [
+            titles.leadingAnchor.constraint(equalTo: iconOfTitle.trailingAnchor, constant: 16),
+            titles.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -40),
+            titles.topAnchor.constraint(equalTo: contentView.topAnchor),
+            titles.bottomAnchor.constraint(equalTo: contentView.bottomAnchor),
+        ]
+        
+        let rightArrowConstraints = [
+            rightArrow.topAnchor.constraint(equalTo: contentView.layoutMarginsGuide.topAnchor),
+            rightArrow.bottomAnchor.constraint(equalTo: contentView.layoutMarginsGuide.bottomAnchor),
+            rightArrow.trailingAnchor.constraint(equalTo: contentView.layoutMarginsGuide.trailingAnchor),
+        ]
+        
+        NSLayoutConstraint.activate(myimageConstraints)
+        NSLayoutConstraint.activate(mylabelConstraints)
+        NSLayoutConstraint.activate(rightArrowConstraints)
+        
+        
+    }
+
+}
