cb02f327a845a3c4b924f2e53db341593dd00063
abkubatt <abkubatt@gmail.com>
2023-03-15 00:07:38 +0600

    .

---
+++ Manas-Menu/Views/MyDeleteTableViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  MyTableViewCell.swift
+//  Manas-Menu
+//
+//  Created by Abdulmajit Kubatbekov on 14.03.23.
+//
+
+import UIKit
+
+class MyDeleteTableViewCell: UITableViewCell {
+    
+    static let identifier = "MyDeleteTableViewCell"
+    
+    
+}
