6b4066a99377b38739a905ee7b356d27f7309f9a
abkubatt <abkubatt@gmail.com>
2023-03-15 00:07:30 +0600

    added Delete VC to canteen and free foods

--- Manas-Menu/Controllers/Other/DeleteCanteenFoodsViewController.swift
+++ Manas-Menu/Controllers/Other/DeleteCanteenFoodsViewController.swift
@@ -9,9 +9,78 @@ import UIKit
 
 class DeleteCanteenFoodsViewController: UIViewController {
 
+    var items = ["Coca Cola", "Fanta", "Pepsi", "Kofte", "Pizza", "Yougurt","Coca Cola", "Fanta", "Pepsi", "Kofte", "Pizza", "Yougurt","Coca Cola", "Fanta", "Pepsi", "Kofte", "Pizza", "Yougurt",]
+    
+    let tableView: UITableView = {
+        let table = UITableView()
+        table.translatesAutoresizingMaskIntoConstraints = false
+        table.register(MyDeleteTableViewCell.self, forCellReuseIdentifier: MyDeleteTableViewCell.identifier)
+        return table
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "Delete Canteen Foods"
+        view.backgroundColor = .systemBackground
+        view.addSubviews(tableView)
+        tableView.dataSource = self
+        tableView.delegate = self
+        
+        configureConstraints()
+    }
+    
+    private func configureConstraints(){
+        
+        let tableViewConstraints = [
+            tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ]
+        
+        NSLayoutConstraint.activate(tableViewConstraints)
     }
 
+}
+
+
+extension DeleteCanteenFoodsViewController: UITableViewDelegate, UITableViewDataSource{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: MyDeleteTableViewCell.identifier, for: indexPath) as? MyDeleteTableViewCell else{
+            return UITableViewCell()
+        }
+
+        cell.textLabel?.textAlignment = .center
+        cell.textLabel?.font = UIFont.systemFont(ofSize: 22)
+        cell.heightAnchor.constraint(equalToConstant: 50).isActive = true
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+
+
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCell.EditingStyle {
+        return .delete
+    }
+
+    // Handle deleting rows
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            tableView.beginUpdates()
+            items.remove(at: indexPath.row)
+            tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.endUpdates()
+        }
+    }
+    
 }
