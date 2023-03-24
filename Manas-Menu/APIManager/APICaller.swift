1104b441dc9ff8a9a4c3394a936f0cee25c1f2e2
abkubatt <abkubatt@gmail.com>
2023-03-24 17:25:19 +0600

    added POST and PUT  methods

--- Manas-Menu/APIManager/APICaller.swift
+++ Manas-Menu/APIManager/APICaller.swift
@@ -39,8 +39,12 @@ class APICaller {
     static let shared = APICaller()
     // "https://drink.free.beeceptor.com/drink"
     func getTrendingMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
-        self.getttt { result in
-            print("---------------------------------------\(result)")
+//        self.getttt { result in
+//            print("---------------------------------------\(result)")
+//        }
+        let menu = Mennu(id: 12, image: "udpated", name: "name up", type: "dinner", calorie: 312)
+        self.putRequest(url: URL(string: "http://192.168.241.114:8080/api/Menus/12")!, menu: menu) { res in
+            print("-------------------\(res)")
         }
         
         guard let url = URL(string: "https://abkubatt.free.beeceptor.com/drinks") else {return}
@@ -74,6 +78,121 @@ class APICaller {
         }
         task.resume()
     }
+    
+   
+
+    func postRequest() {
+
+      // declare the parameter as a dictionary that contains string as key and value combination. considering inputs are valid
+
+      let parameters: [String: Any] = ["id": 13, "name": "jack"]
+
+      // create the url with URL
+      let url = URL(string: "http://192.168.241.114:8080/api/Menus")! // change server url accordingly
+
+      // create the session object
+      let session = URLSession.shared
+
+      // now create the URLRequest object using the url object
+      var request = URLRequest(url: url)
+      request.httpMethod = "PUT" //set http method as POST
+
+      // add headers for the request
+      request.addValue("application/json", forHTTPHeaderField: "Content-Type") // change as per server requirements
+      request.addValue("application/json", forHTTPHeaderField: "Accept")
+        let menu = Mennu(id: 12, image: "Abdulmajit IMAGE", name: "ASH", type: "Main", calorie: 234)
+
+      do {
+        // convert parameters to Data and assign dictionary to httpBody of request
+//        request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: .prettyPrinted)
+          let encoder = JSONEncoder()
+              guard let httpBody = try? encoder.encode(menu) else {
+                  return
+              }
+              request.httpBody = httpBody
+      } catch let error {
+        print(error.localizedDescription)
+        return
+      }
+
+      // create dataTask using the session object to send data to the server
+      let task = session.dataTask(with: request) { data, response, error in
+
+        if let error = error {
+          print("Post Request Error: \(error.localizedDescription)")
+          return
+        }
+
+        // ensure there is valid response code returned from this HTTP response
+        guard let httpResponse = response as? HTTPURLResponse,
+              (200...299).contains(httpResponse.statusCode)
+        else {
+          print("Invalid Response received from the server")
+          return
+        }
+
+        // ensure there is data returned
+        guard let responseData = data else {
+          print("nil Data received from the server")
+          return
+        }
+
+        do {
+          // create json object from data or use JSONDecoder to convert to Model stuct
+          if let jsonResponse = try JSONSerialization.jsonObject(with: responseData, options: .mutableContainers) as? [String: Any] {
+            print(jsonResponse)
+            // handle json response
+          } else {
+            print("data maybe corrupted or in wrong format")
+            throw URLError(.badServerResponse)
+          }
+        } catch let error {
+          print(error.localizedDescription)
+        }
+      }
+      // perform the task
+      task.resume()
+    }
+    
+    
+    
+    func putRequest(url: URL, menu: Mennu, completion: @escaping (Result<Void, Error>) -> Void) {
+        var request = URLRequest(url: url)
+        request.httpMethod = "PUT"
+
+        do {
+            let jsonData = try JSONEncoder().encode(menu)
+            request.httpBody = jsonData
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+
+            let session = URLSession.shared
+            let task = session.dataTask(with: request) { data, response, error in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+
+                guard let httpResponse = response as? HTTPURLResponse else {
+                    completion(.failure(NSError(domain: "Invalid response", code: 0, userInfo: nil)))
+                    return
+                }
+
+                if (200...299).contains(httpResponse.statusCode) {
+                    completion(.success(()))
+                } else {
+                    let error = NSError(domain: "HTTP Error", code: httpResponse.statusCode, userInfo: nil)
+                    completion(.failure(error))
+                }
+            }
+            task.resume()
+        } catch {
+            completion(.failure(error))
+        }
+    }
+
+
+
+
     
     //"https://drink.free.beeceptor.com/desserts"
     func getUpcomingMovies(completion: @escaping (Result<[Title], Error>) -> Void) {
